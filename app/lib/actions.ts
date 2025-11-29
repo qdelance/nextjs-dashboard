a1f4dda764ad726a865ea7877193e3fe32a4ca38
Quentin Delance <quentin.delance@gmail.com>
2025-11-29 21:16:45 +0100

    Revert "Try to fix build on Vercel"
    This reverts commit 95fa47220e628da914e50f20c1da112807826c90.

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -20,7 +20,6 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  'use server'
   console.log('Create invoice submitted values', formData);
 
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -51,7 +50,6 @@ export async function createInvoice(formData: FormData) {
 }
 
 export async function updateInvoice(id: string, formData: FormData) {
-  'use server'
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -77,7 +75,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  'use server'
   // test errors
   // throw new Error('Failed to Delete Invoice');
 
